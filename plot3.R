19001b1355ba96ee0e6a5ff12ca992a50ff3571e
ravirathi <ravirathi7@yahoo.co.in>
2015-04-26 00:51:41 -0500

    Update plot3.R
    plot3.R

--- plot3.R
+++ plot3.R
@@ -12,7 +12,7 @@ by_year <- group_by(select(NEI_Baltimore,year, type, Emissions),year, type)
 
 EmDet <- summarise(by_year, TotEm = sum(Emissions))
 
-png("Proj2_plot3.png", width = 480, height = 480)
+png("plot3.png", width = 480, height = 480)
 
 ggplot(EmDet, aes(year,TotEm))+ geom_point() + geom_line(aes(color = factor(type))) +ggtitle('Emissions per Type in Baltimore City, Maryland') +ylab(expression(paste('Log', ' of PM'[2.5], ' Emissions'))) +scale_x_continuous(breaks= c(1999, 2002, 2005, 2008))
  
